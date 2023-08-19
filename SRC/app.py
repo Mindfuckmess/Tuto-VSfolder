ab9e3110a61e43068b7a6fd974892376cf38ff9c
Mindfuckmess <142143639+Mindfuckmess@users.noreply.github.com>
2023-08-19 11:46:56 +0200

    Add files via upload

---
+++ SRC/app.py
@@ -0,0 +1,21 @@
+print ("hola mundo")
+#Esto es un comentario que no vale para nada
+#y esto es para demostrar que puedo poner varias líneas de comentarios
+x = 5; y = 4; sum = x + y; print(sum)
+name = "Dani" 
+surname = "Urbina"
+print(name, surname)
+print("Hoy hacemos un", "6" ,sum)
+Frutas= ("orange",
+        "mango",
+        "plátano")
+print(Frutas) #y así hago la lista de la compra
+if 5 > 4:
+    x = "su puta madre"
+    print (x)
+print ("el bloque termina aquí")
+#Las VV solo pueden empezar por número, letra o _
+#Las VV solo pueden contener número, letra o _
+_Nombre = "Juan"
+Age= "21"
+print(_Nombre, Age)
